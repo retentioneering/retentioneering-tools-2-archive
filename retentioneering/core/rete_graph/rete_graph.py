01224eb31cd415a34b8497e70728dfdb9dbfe4d3
Alexey Avramenko <2822698@gmail.com>
2021-10-27 19:33:21 +0700

    debug

--- retentioneering/core/rete_graph/rete_graph.py
+++ retentioneering/core/rete_graph/rete_graph.py
@@ -527,7 +527,7 @@ class ReteGraph():
                 return self._to_json(settings[name])
             return "undefined"
 
-        print("render graph2")
+        print("render graph3")
 
         init_graph_js = templates.__INIT_GRAPH__.format(
             server_id="'" + self.server.id + "'",
